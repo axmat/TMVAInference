ac95d3c7cf909faa410d604166647ce6e72b1f82
axmat <amthamdan@gmail.com>
2021-06-20 23:22:44 +0200

    Implement the forward pass of LSTM

--- ROperatorLSTM.hxx
+++ ROperatorLSTM.hxx
@@ -96,33 +96,9 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
    // Activation functions
    if (fActivations.empty()) {
       if (fDirection == "forward" || fDirection == "backward") {
-         fActivations = {"Tanh"};
+         fActivations = {"Sigmoid", "Tanh", "Tanh"};
       } else {
-         fActivations = {"Tanh", "Tanh"};
-      }
-   }
-   // Default values of alpha and beta
-   for (size_t i = 0; i < fActivations.size(); i++) {
-      if (fActivations[i] == "LeakyRelu") {
-         if (fActivationAlpha.size() < i+1)
-            fActivationAlpha.push_back(0.01);
-         if (fActivationBeta.size() < i+1)
-            fActivationBeta.push_back(0.0);
-      } else if (fActivations[i] == "ThresholdedRelu") {
-         if (fActivationAlpha.size() < i+1)
-            fActivationAlpha.push_back(0.1);
-         if (fActivationBeta.size() < i+1)
-            fActivationBeta.push_back(0.0);
-      } else if (fActivations[i] == "HardSigmoid") {
-          if (fActivationAlpha.size() < i+1)
-            fActivationAlpha.push_back(.2);
-          if (fActivationBeta.size() < i+1)
-            fActivationBeta.push_back(.5);
-      } else if (fActivations[i] == "Elu") {
-         if (fActivationAlpha.size() < i+1)
-            fActivationAlpha.push_back(1.);
-         if (fActivationBeta.size() < i+1)
-            fActivationBeta.push_back(0.0);
+         fActivations = {"Sigmoid", "Tanh", "Tanh", "Sigmoid", "Tanh", "Tanh"};
       }
    }
 
@@ -156,8 +132,8 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
          for (size_t direction = 0; direction < num_directions; direction++) {
             // Compute the bias_xh + bias_hh
             for (size_t h = 0; h < fHiddenSize; h++) {
-               sum[h] = B.GetData()[direction * 4 * fHiddenSize + h] +
-                  B.GetData()[direction * 4 * fHiddenSize + fHiddenSize + h];
+               sum[h] = B.GetData()[direction * 8 * fHiddenSize + gate * fHiddenSize + h] +
+                  B.GetData()[direction * 8 * fHiddenSize + gate * fHiddenSize + h + 4*fHiddenSize];
             }
             // Copy sum into bias
             for (size_t seq = 0; seq < seq_length; seq++) {
@@ -179,9 +155,9 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
          initial_hidden_state = initial_h.GetData();
       } else {
          initial_hidden_state = new T[num_directions * batch_size * fHiddenSize];
-         for (size_t direction=0; direction < num_directions; direction++) {
-            for (size_t batch=0; batch < batch_size; batch++) {
-               for (size_t h=0; h < fHiddenSize; h++) {
+         for (size_t direction = 0; direction < num_directions; direction++) {
+            for (size_t batch = 0; batch < batch_size; batch++) {
+               for (size_t h = 0; h < fHiddenSize; h++) {
                   initial_hidden_state[direction * batch_size * fHiddenSize + batch * fHiddenSize + h] =
                      initial_h.GetData()[batch * num_directions * fHiddenSize + direction * fHiddenSize
                         + h];
@@ -197,9 +173,9 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
          initial_cell_state = initial_c.GetData();
       } else {
          initial_cell_state = new T[num_directions * batch_size * fHiddenSize];
-         for (size_t direction=0; direction < num_directions; direction++) {
-            for (size_t batch=0; batch < batch_size; batch++) {
-               for (size_t h=0; h < fHiddenSize; h++) {
+         for (size_t direction = 0; direction < num_directions; direction++) {
+            for (size_t batch = 0; batch < batch_size; batch++) {
+               for (size_t h = 0; h < fHiddenSize; h++) {
                   initial_cell_state[direction * batch_size * fHiddenSize + batch * fHiddenSize + h] =
                      initial_c.GetData()[batch * num_directions * fHiddenSize + direction * fHiddenSize
                         + h];
@@ -215,13 +191,15 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
    size_t hidden_state_size = seq_length * num_directions * batch_size * fHiddenSize;
    T input_gate[hidden_state_size];
    T* forget_gate = nullptr;
-   if (fInputForget == 1) {
+   if (fInputForget == 0) {
       forget_gate = new T[hidden_state_size];
    }
    T output_gate[hidden_state_size];
-   T cell_input[hidden_state_size];
+   T cell_gate[hidden_state_size];
+   T* gates[4] = {input_gate, forget_gate, output_gate, cell_gate};
    // Set the cell state
    T cell_state[hidden_state_size];
+   T new_cell_state[hidden_state_size];
    // Set the hidden state
    T * hidden_state = nullptr;
    if (fLayout == 0 && Y.GetShape().size() > 0) {
@@ -230,11 +208,9 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
       hidden_state = new T[hidden_state_size];
    }
 
-   T* gates[4] = {input_gate, forget_gate, output_gate, cell_input};
-
    for (size_t direction = 0; direction < num_directions; direction++) {
       for (size_t gate = 0; gate < 4; gate++) {
-         if (gate == 1 && fInputForget == 0)
+         if (gate == 1 && fInputForget == 1)
             continue;
          // feedforward = input * weight^T + bias
          char transA = 'N';
@@ -244,7 +220,8 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
          int k = input_size;
          float alpha = 1.;
          float beta = 0.;
-         size_t w_offset = (direction * 4 + gate) * fHiddenSize * input_size;
+         size_t w_offset = direction * 4 * fHiddenSize * input_size +
+            gate * fHiddenSize * input_size;
          BLAS::sgemm_(&transB, &transA, &n, &m, &k, &alpha, W.GetData() + w_offset, &k,
             input, &k, &beta, feedforward, &n);
          if (bias) {
@@ -266,15 +243,144 @@ void ROperatorLSTM<T>::Forward_blas(RTensor<T> &X,
          }
       }
 
+      bool backward = (fDirection == "backward") || (direction == 1);
       for (size_t seq = 0; seq < seq_length; seq++) {
-         // gate = 1.0 * gate + previous_hidden_state
-         // Clip the elements of the gate into the range [-fClip, fClip]
-         // gate = sigma(gate)
-         // cell_state = input_gate o cell_input
-         // cell_state = 1.0 * cell_state + forget_gate o previous_cell_state
-         // Clip the elements of cell_state into the range [-fClip, fClip]
-         // cell_state = phi(cell_state)
-         // hidden_state = output_gate o cell_state
+         size_t index = backward ? seq_length - 1 - seq : seq;
+         char transA = 'N';
+         char transB = 'N';
+         int m = batch_size;
+         int n = fHiddenSize;
+         float alpha = 1.;
+         size_t offset = index * num_directions * batch_size * fHiddenSize
+            + direction * batch_size * fHiddenSize;
+         size_t size = batch_size * fHiddenSize;
+         for (size_t gate = 0; gate < 4; gate++) {
+            if (gate == 1 && fInputForget == 1)
+               continue;
+            // gate = 1.0 * gate + previous_hidden_state * R^T
+            if (seq == 0) {
+               if (initial_hidden_state) {
+                  size_t r_offset = direction * 4 * fHiddenSize * fHiddenSize +
+                     gate * fHiddenSize * fHiddenSize;
+                  size_t initial_h_offset = direction * batch_size * fHiddenSize;
+                  BLAS::sgemm_(&transB, &transA, &n, &m, &n, &alpha, R.GetData() + r_offset, &n,
+                     initial_hidden_state + initial_h_offset, &n, &alpha,
+                     gates[gate] + offset, &n);
+               }
+            } else {
+               size_t r_offset = direction * 4 * fHiddenSize * fHiddenSize +
+                  gate * fHiddenSize * fHiddenSize;
+               size_t previous_offset = (backward ? (index + 1) : (seq - 1)) * num_directions * batch_size *
+                  fHiddenSize + direction * batch_size * fHiddenSize;
+               BLAS::sgemm_(&transB, &transA, &n, &m, &n, &alpha, R.GetData() + r_offset, &n,
+                  gates[gate] + previous_offset, &n, &alpha, gates[gate] + offset, &n);
+            }
+         }
+         // TODO Peepholes LSTM
+         for (size_t gate = 0; gate < 3; gate++) {
+            // Clip the elements of the input gate, the forget gate and the output gate into the range [-fClip, fClip]
+            if (fClip > 0.) {
+               for (size_t i = offset; i < offset + size; i++) {
+                  T x = (gates[gate][i] > -fClip) ? gates[gate][i] : -fClip;
+                  gates[gate][i] = (x < fClip)? x : fClip;
+               }
+            }
+            // Apply the activation function to the input gate, the forget gate and the output gate
+            if (fActivations[direction * 3] == "Relu") {
+               for (size_t i = offset; i < offset + size; i++) {
+                  if (gates[gate][i] < 0.)
+                     gates[gate][i] = 0.;
+               }
+            } else if (fActivations[direction * 3] == "Tanh") {
+               for (size_t i = offset; i < offset + size; i++) {
+                  float ex = exp(-2 * gates[gate][i]);
+                  gates[gate][i] = (1. - ex) / (1. + ex);
+               }
+            } else if (fActivations[direction * 3] == "Sigmoid") {
+               for (size_t i = offset; i < offset + size; i++) {
+                  gates[gate][i] = 1. / (1. + exp(-gates[gate][i]));
+               }
+            } else {
+               throw std::runtime_error("TMVA - Activation function " + fActivations[direction * 3] +
+                  " not implemented.");
+            }
+         }
+         // Clip the elements of the cell gate into the range [-fClip, fClip]
+         if (fClip > 0.) {
+            for (size_t i = offset; i < offset + size; i++) {
+               T x = (cell_gate[i] > -fClip) ? cell_gate[i] : -fClip;
+               cell_gate[i] = (x < fClip)? x : fClip;
+            }
+         }
+         // Apply the activation function to the cell gate, cell_gate = g(cell_gate)
+         if (fActivations[direction * 3 + 1] == "Relu") {
+            for (size_t i = offset; i < offset + size; i++) {
+               if (cell_gate[i] < 0.)
+                  cell_gate[i] = 0.;
+            }
+         } else if (fActivations[direction * 3 + 1] == "Tanh") {
+            for (size_t i = offset; i < offset + size; i++) {
+               float ex = exp(-2 * cell_gate[i]);
+               cell_gate[i] = (1. - ex) / (1. + ex);
+            }
+         } else if (fActivations[direction * 3 + 1] == "Sigmoid") {
+            for (size_t i = offset; i < offset + size; i++) {
+               cell_gate[i] = 1. / (1. + exp(-cell_gate[i]));
+            }
+         } else {
+            throw std::runtime_error("TMVA - Activation function " + fActivations[direction * 3 + 1] +
+               " not implemented.");
+         }
+         // cell_state = input_gate o cell_gate
+         for (size_t i = offset; i < offset + size; i++) {
+            cell_state[i] = input_gate[i] * cell_gate[i];
+         }
+         if (fInputForget == 0) {
+            if (seq == 0) {
+               // cell_state += forget_gate o initial_cell_state
+               for (size_t i = offset; i < offset + size; i++) {
+                  cell_state[i] += forget_gate[i] * initial_cell_state[i];
+               }
+            } else {
+               // cell_state += forget_gate o previous_cell_state
+               size_t previous_offset;
+               for (size_t i = 0; i < size; i++) {
+                  cell_state[i + offset] += forget_gate[i + offset] * cell_state[i + previous_offset];
+               }
+            }
+         }
+         // copy cell_state into new_cell_state
+         std::copy(cell_state + offset, cell_state + offset + size, new_cell_state + offset);
+         // Clip the elements of new_cell_state into the range [-fClip, fClip]
+         if (fClip > 0.) {
+            for (size_t i = offset; i < offset + size; i++) {
+               T x = (new_cell_state[i] > -fClip) ? new_cell_state[i] : -fClip;
+               new_cell_state[i] = (x < fClip) ? x : fClip;
+            }
+         }
+         // new_cell_state = h(new_cell_state)
+         if (fActivations[direction * 3 + 2] == "Relu") {
+            for (size_t i = offset; i < offset + size; i++) {
+               if (new_cell_state[i] < 0.)
+                  new_cell_state[i] = 0.;
+            }
+         } else if (fActivations[direction * 3 + 2] == "Tanh") {
+            for (size_t i = offset; i < offset + size; i++) {
+               float ex = exp(-2 * new_cell_state[i]);
+               new_cell_state[i] = (1. - ex) / (1. + ex);
+            }
+         } else if (fActivations[direction * 3 + 2] == "Sigmoid") {
+            for (size_t i = offset; i < offset + size; i++) {
+               new_cell_state[i] = 1. / (1. + exp(-new_cell_state[i]));
+            }
+         } else {
+            throw std::runtime_error("TMVA - Activation function " + fActivations[direction * 3 + 2] +
+               " not implemented.");
+         }
+         // hidden_state = output_gate o new_cell_state
+         for (size_t i = offset; i < offset + size; i++) {
+            hidden_state[i] = output_gate[i] * new_cell_state[i];
+         }
       }
    }
 
